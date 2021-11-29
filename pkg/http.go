f5b6230fb0d7d8996589c3d4f37b84d602c8f5e9
Daniil Lee <24758309+leedaniil@users.noreply.github.com>
2021-11-29 16:19:33 +0300

    Add func for GET HTTP request

---
+++ pkg/http.go
@@ -0,0 +1,33 @@
+package pkg
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/pkg/errors"
+)
+
+func GetHTTPResponse(url string, v interface{}) error {
+	res, err := http.Get(url)
+	if err != nil {
+		return errors.Wrap(err, "failed to make GET request")
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return errors.New("failed to obtain json")
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return errors.Wrap(err, "failed to read bytes from body")
+	}
+
+	err = json.Unmarshal(body, v)
+	if err != nil {
+		return errors.Wrap(err, "failed to unmarshal json")
+	}
+
+	return nil
+}
