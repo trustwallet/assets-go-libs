be983932e02e6020f3c8449e7b4a00edd3dcb2b7
Daniel <24758309+leedaniil@users.noreply.github.com>
2021-11-29 20:11:33 +0300

    Remove pkg/errors dependency, fix getting external asset info req (#18)

--- pkg/http.go
+++ pkg/http.go
@@ -2,32 +2,31 @@ package pkg
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
-
-	"github.com/pkg/errors"
 )
 
 // nolint: noctx
 func GetHTTPResponse(url string, v interface{}) error {
 	res, err := http.Get(url)
 	if err != nil {
-		return errors.Wrap(err, "failed to make GET request")
+		return fmt.Errorf("failed to make GET request: %v", err)
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
-		return errors.New("failed to obtain json")
+		return fmt.Errorf("failed to obtain json: %v", err)
 	}
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return errors.Wrap(err, "failed to read bytes from body")
+		return fmt.Errorf("failed to read bytes from body: %v", err)
 	}
 
 	err = json.Unmarshal(body, v)
 	if err != nil {
-		return errors.Wrap(err, "failed to unmarshal json")
+		return fmt.Errorf("failed to unmarshal json: %v", err)
 	}
 
 	return nil
@@ -37,7 +36,7 @@ func GetHTTPResponse(url string, v interface{}) error {
 func GetHTTPResponseCode(url string) (int, error) {
 	res, err := http.Get(url)
 	if err != nil {
-		return 0, errors.Wrap(err, "failed to make GET request")
+		return 0, fmt.Errorf("failed to make GET request: %v", err)
 	}
 	defer res.Body.Close()
 
