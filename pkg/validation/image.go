7bf02580b8f188fd26f96eb6334152a5edd04ea6
Daniel <24758309+leedaniil@users.noreply.github.com>
2021-12-07 17:23:49 +0300

    Add temp image validation for CI (#26)

--- pkg/validation/image.go
+++ pkg/validation/image.go
@@ -17,6 +17,24 @@ const (
 	minH = 128
 )
 
+// TODO: Fix all logos in "assets" and then we can use ValidatePngImageDimension in CI.
+// Old logo's have dimensions like 195x163, 60x60 and etc. This method is used only in CI.
+// ValidatePngImageDimensionForCI should be removed after logo's fixing.
+func ValidatePngImageDimensionForCI(file io.Reader) error {
+	img, err := png.DecodeConfig(file)
+	if err != nil {
+		return fmt.Errorf("failed to decode image: %w", err)
+	}
+
+	// TODO: If we fix all incorrect logos in assets repo, we could use "|| img.Width != img.Height" in addition.
+	if img.Width > maxW || img.Height > maxH || img.Height < minH {
+		return fmt.Errorf("%w: max - %dx%d, min - %dx%d; given %dx%d",
+			ErrInvalidImgDimension, maxW, maxH, minW, minH, img.Width, img.Height)
+	}
+
+	return nil
+}
+
 func ValidatePngImageDimension(file io.Reader) error {
 	img, err := png.DecodeConfig(file)
 	if err != nil {
