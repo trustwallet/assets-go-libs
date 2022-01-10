08b3197ead2a166625288696aa492b02091c48bb
iamdeadlyz <29373963+Iamdeadlyz@users.noreply.github.com>
2022-01-10 22:23:23 +0800

    Added validateDuplicateKeys function
    Checks for duplicate keys in a json file

--- validation/bytes.go
+++ validation/bytes.go
@@ -2,11 +2,66 @@ package validation
 
 import (
 	"encoding/json"
+	"fmt"
+	"strconv"
 )
 
 func ValidateJson(b []byte) error {
 	if !json.Valid(b) {
 		return ErrInvalidJson
+	}
+
+	return nil
+}
+
+func validateDuplicateKeys(d *json.Decoder, path []string) error {
+	mainToken, err := d.Token()
+	if err != nil {
+		return err
+	}
+
+	delimiter, ok := mainToken.(json.Delim)
+
+	if !ok {
+		return nil
+	}
+
+	if delimiter == '{' {
+		keys := make(map[string]bool)
+		for d.More() {
+			theToken, err := d.Token()
+			if err != nil {
+				return err
+			}
+
+			key := theToken.(string)
+
+			if _, exists := keys[key]; exists {
+				return fmt.Errorf("duplicate key: %s", key)
+			}
+			keys[key] = true
+
+			if err := validateDuplicateKeys(d, append(path, key)); err != nil {
+				return fmt.Errorf("invalid value on key: %s", key)
+			}
+
+		}
+		if _, err := d.Token(); err != nil {
+			return err
+		}
+	} else if delimiter == '[' {
+		counter := 0
+		for d.More() {
+			if err := validateDuplicateKeys(d, append(path, strconv.Itoa(counter))); err != nil {
+				return err
+			}
+
+			counter++
+		}
+
+		if _, err := d.Token(); err != nil {
+			return err
+		}
 	}
 
 	return nil
