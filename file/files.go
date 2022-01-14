6349bae9792605f8aab62699ee202ffe45a36373
Daniel <24758309+leedaniil@users.noreply.github.com>
2022-01-14 16:44:29 +0300

    Add new file func and update libs (#58)
    * Add a func for creating files by full path
    
    * Update go-libs and go-primitives

--- file/files.go
+++ file/files.go
@@ -16,3 +16,11 @@ func CreateDirPath(path string) error {
 
 	return os.MkdirAll(dirPath, os.ModePerm)
 }
+
+func CreateFileWithPath(p string) (*os.File, error) {
+	if err := os.MkdirAll(filepath.Dir(p), 0770); err != nil {
+		return nil, err
+	}
+
+	return os.Create(p)
+}
