c8863a7a0bc9a6cd880bfad25e3d9145aa39970a
daniel <24758309+leedaniil@users.noreply.github.com>
2022-01-27 23:26:42 +0300

    Add assets-manager client (#68)

---
+++ client/assets-manager/client.go
@@ -0,0 +1,29 @@
+package assetsmanager
+
+import (
+	"time"
+
+	"github.com/trustwallet/go-libs/client"
+)
+
+type Client struct {
+	req client.Request
+}
+
+func InitClient(url string, errorHandler client.HttpErrorHandler) Client {
+	return Client{
+		req: client.InitJSONClient(url, errorHandler),
+	}
+}
+
+func (c *Client) ValidateAssetInfo(req *AssetValidationReq) (result AssetValidationResp, err error) {
+	err = c.req.Post(&result, "/v1/validate/asset_info", req)
+
+	return result, err
+}
+
+func (c *Client) GetTagValues() (result TagValuesResp, err error) {
+	err = c.req.GetWithCache(&result, "/v1/values/tags", nil, time.Hour)
+
+	return result, err
+}
--- go.mod
+++ go.mod
@@ -4,13 +4,11 @@ go 1.17
 
 require github.com/trustwallet/go-primitives v0.0.24
 
-require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
-)
+require github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 
 require (
 	github.com/deckarep/golang-set v1.7.1 // indirect
+	github.com/trustwallet/go-libs v0.2.25
 	golang.org/x/crypto v0.0.0-20211209193657-4570a0811e8b // indirect
 	golang.org/x/image v0.0.0-20211028202545-6944b10bf410
 	golang.org/x/sys v0.0.0-20211213223007-03aa0b5f6827 // indirect
